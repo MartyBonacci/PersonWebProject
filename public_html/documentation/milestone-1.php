858221768e1805a5dc1a960f6f235a4c42975a0e
CodeCanna <fleabass4@gmail.com>
2019-07-23 11:00:22 -0600

    Here is my PWP

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>PWP Document 1</title>
+	</head>
+	<body>
+		<h1>PWP Documentation for Milestone 1</h1>
+		<div id="purpose">
+			<h3>Purpose of my PWP</h3>
+			<ol>
+				<li>To Impress Employers</li>
+				<il>To show off my weg skills.</il>
+				<li>Expanding my portfolio.</li>
+			</ol>
+		</div>
+		<div id="audience">
+			<h2>Target audience</h2>
+			<p>Anyone who is interested in my skill set as a developer.  They can get on this site and see my skills and what I know.</p>
+		</div>
+		<div id="goal">
+			<ol>
+				<li>Help get employment.</li>
+				<li>To show potential employers my skills.</li>
+				<li>Get a nice dynamic web page.</li>
+			</ol>
+		</div>
+		<div id="persona">
+			<h2>Persona</h2>
+			<p>A potential employer who gets on my site.  He is looking for a certain skill set, and I aim to have the skills proved on my site.</p>
+		</div>
+	</body>
+</html>
