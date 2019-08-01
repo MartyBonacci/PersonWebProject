62a21832c0ac6cfd6c3f0cbe08a130ea96b08e6f
CodeCanna <fleabass4@gmail.com>
2019-07-31 23:35:47 -0600

    Added more list for content

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -18,5 +18,22 @@
             <li></li>
         </ul>
     </section>
+	<section>
+		<h3>Body:</h3>
+		<ul>
+			<li>Content about myself</li>
+			<li>Links to sites I have done</li>
+			<li>A little animation?</li>
+			<li>What I want to accomplish</li>
+		</ul>
+	</section>
+	<section>
+		<h3>Footer:</h3>
+		<ul>
+			<li>Menu Links</li>
+			<li>Form</li>
+			<li>Capta</li>
+		</ul>
+	</section>
 </body>
 </html>
