c9ef3655eaa3ced4f668671e68006901081b8f4d
CodeCanna <fleabass4@gmail.com>
2019-08-01 08:14:12 -0600

    Added images and completed assignment

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -10,16 +10,32 @@
     <section>
         <h1>Content to be featured</h1>
 
-        <h3>Header:</h3>
+		 <h3>Header:</h3>
+
+		 <p>
+			 In the header there will be a title of some sort, a logo, and an interactive and mobile compatible nav menu.<br>
+			 The menu will have links to other websites I have done as well as a link to my GitHub and LinkdIn.  A logo that will represent me as <br>
+			 a developer.  The nav menu will be mobile and desktop compatible, and have a fluid feel and animations.<br>
+			 I would like the make the nav sticky.
+		 </p>
+
+
         <ul>
             <li>Page title</li>
             <li>My logo</li>
-            <li>A nav menu</li>
-            <li></li>
+            <li>A nav menu interactive and adaptable</li>
         </ul>
     </section>
 	<section>
 		<h3>Body:</h3>
+
+		<p>
+			The body will contain personal content and professional content about me.  I would like to make this almost like a resume.<br>
+			If possible I would like to get a small animation or piece of art that I done.  Most importantly I would like to add what I hope<br>
+			to accomplish in my developer career.  I would like for this to be a drop down nest type menu, to feature my javascript and CSS skills.<br>
+			I would like to have a sample of each skill listed here.
+		</p>
+
 		<ul>
 			<li>Content about myself</li>
 			<li>Links to sites I have done</li>
@@ -29,11 +45,27 @@
 	</section>
 	<section>
 		<h3>Footer:</h3>
+
+		<p>
+			The footer will contain some of the same links as the nav as well as a type of copyright?  A form and capta will also be featured in<br>
+			the footer.  The form will take a name password and email, and the site will send the user an email of the entered information.<br>
+			The footer will not be sticky.
+		</p>
+
 		<ul>
 			<li>Menu Links</li>
 			<li>Form</li>
 			<li>Capta</li>
 		</ul>
+	</section>
+	<section>
+		<h1>Wireframe:</h1>
+		<h3>Mobile</h3>
+		<img src="docimgs/mobile.jpg" alt="Mobile Wireframe" width="500" height="auto"/>
+
+		<h3>Desktop</h3>
+		<img src="docimgs/20190801_002344.jpg" alt="Desktop Wireframe" width="500" height="auto"/>
+		<img src="docimgs/20190801_002355.jpg" alt="Desktop Wireframe" width="500" height="auto"/>
 	</section>
 </body>
 </html>
