e446bf153572646534ee346c182576b3120fda47
CodeCanna <fleabass4@gmail.com>
2019-07-31 19:10:07 -0600

    Added header list of content.

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <section>
+        <h1>Content to be featured</h1>
+
+        <h3>Header:</h3>
+        <ul>
+            <li>Page title</li>
+            <li>My logo</li>
+            <li>A nav menu</li>
+            <li></li>
+        </ul>
+    </section>
+</body>
+</html>
